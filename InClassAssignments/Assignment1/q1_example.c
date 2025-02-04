603e3a852098ea16c457787d10b9f076d2079aca
akloter24 <akloter8@gmail.com>
2025-02-04 17:43:05 +0000

    init: buffer overflow example

---
+++ InClassAssignments/Assignment1/q1_example.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+    void reverseString(char str[]) {
+        short length = strlen(str);
+        for (int i = 0; i < length / 2; i++) {
+            char temp = str[i];
+            str[i] = str[length - i - 1];
+            str[length - i - 1] = temp;
+        }
+    }
+
+    int main() {
+        char str[20];
+        printf("Enter a string: ");
+        fgets(str, sizeof(str), stdin);
+        reverseString(str);
+        printf("Reversed string: %s\n", str);
+        return 0;
+    }
