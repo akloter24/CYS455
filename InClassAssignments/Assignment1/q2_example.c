c0083635514d01c8f70f303480141b56bad56565
akloter24 <akloter8@gmail.com>
2025-02-04 18:24:49 +0000

    fix: removed buffer overflow vulnerability

--- InClassAssignments/Assignment1/q2_example.c
+++ InClassAssignments/Assignment1/q2_example.c
@@ -2,19 +2,21 @@
 #include <string.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
-    char buffer[5];
-    //a prompt how to execute the program
-    if (argc < 2)
-    {
+int main(int argc, char *argv[]) {
+    char buffer[5];  // Buffer of 5 bytes
+
+    // Check if the user provided an argument
+    if (argc < 2) {
         printf("strcpy() NOT executed...\n");
-        printf("Syntax:%s<characters>/n",argv[0]);
+        printf("Syntax: %s <characters>\n", argv[0]);
         exit(0);
     }
-    //copy the user input to mybuffer
-    strcpy(buffer, argv[1]);
+
+    // Use snprintf to safely copy and format the string
+    snprintf(buffer, sizeof(buffer), "%s", argv[1]);
+
     printf("buffer content = %s\n", buffer);
-    printf("strcpy() executed...\n");
+    printf("snprintf() executed safely...\n");
+
     return 0;
 }
