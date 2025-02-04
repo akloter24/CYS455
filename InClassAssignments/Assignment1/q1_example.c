0eae250c848ef43fc73d1c18b00be2e9f3e92368
akloter24 <akloter8@gmail.com>
2025-02-04 18:24:34 +0000

    fix: removed buffer overflow vulnerability

--- InClassAssignments/Assignment1/q1_example.c
+++ InClassAssignments/Assignment1/q1_example.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
 #include <string.h>
 
-    void reverseString(char str[]) {
-        short length = strlen(str);
-        for (int i = 0; i < length / 2; i++) {
-            char temp = str[i];
-            str[i] = str[length - i - 1];
-            str[length - i - 1] = temp;
-        }
+void reverseString(char str[]) {
+    short length = strlen(str);
+    for (int i = 0; i < length / 2; i++) {
+        char temp = str[i];
+        str[i] = str[length - i - 1];
+        str[length - i - 1] = temp;
     }
+}
 
-    int main() {
-        char str[20];
-        printf("Enter a string: ");
-        fgets(str, sizeof(str), stdin);
+int main() {
+    char str[20];  // Buffer size is 20
+    printf("Enter a string: ");
+    
+    // Use fgets to prevent buffer overflow
+    if (fgets(str, sizeof(str), stdin) != NULL) {
+        // Remove the trailing newline if present
+        str[strcspn(str, "\n")] = '\0';
+
         reverseString(str);
         printf("Reversed string: %s\n", str);
-        return 0;
+    } else {
+        printf("Error reading input.\n");
     }
+    
+    return 0;
+}
