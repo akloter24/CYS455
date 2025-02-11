dd2784d187852aeb0c289675c8387422242940ca
akloter24 <akloter8@gmail.com>
2025-02-11 20:59:22 +0000

    fix:renaming folders

--- InClassAssignments/Assignment3/fstring2.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-void main(int argc, char **argv){
-    char buf[100];
-    //snprintf(buf, sizeof buf, argv[1]);
-    snprintf(buf, sizeof buf, "%s", argv[1]);
-    printf("Buffer Content: %s\n", buf );
-}
