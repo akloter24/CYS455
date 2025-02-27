2439da6a49e4796a11381f97fa8e3cb271fc582b
akloter24 <akloter8@gmail.com>
2025-02-27 21:05:53 +0000

    feat: more randomization

---
+++ InClassAssignments/Randomization/random5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <assert.h>
+
+int isprime(int n){
+    int i;
+    if( n % 2 == 0 || n == 1 ) { return 0; }
+    for (i = 3; i*i <= n; i += 2 ){
+        if(n % i == 0) {return 0;}
+    }
+    return 1;
+}
+//return a uniform random value in the range 0.. n-1 inclusive
+int randPrime(void){
+    int r;
+    //extra parens avoid warnings
+    while(!isprime((r=rand())));
+    return r;
+}
+int main(int argc, char **argv){
+    int i;
+    srand(time(0));
+    for(i = 0; i < 10; i++){
+        printf("%d\n", randPrime());    
+    }
+    return 0;
+}
